70b613d7c73132df88706fdcda678b27c4306c16
Anne-gaëlle Bernard <anne-gaelle.bernard@laplateforme.io>
2026-01-28 16:30:53 +0100

    frontend avec un lien vers la page de file d'attente qui est composer de input nom+selectmenu+capacité de rejoindre ou quitter la file d'attente

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+
+let queue = [];
+let id = 0;
+
+io.on("connection", (socket) => {
+  socket.emit("update", queue);
+
+  socket.on("join", (data) => {
+    queue.push({ id: id++, name: data.name, food: data.food });
+    io.emit("update", queue);
+  });
+
+  socket.on("leave", (personId) => {
+    queue = queue.filter(p => p.id !== personId);
+    io.emit("update", queue);
+  });
+
+  socket.on("updateFood", (data) => {
+    let person = queue.find(p => p.id === data.id);
+    if (person) person.food = data.food;
+    io.emit("update", queue);
+  });
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: 40 }}>
+      <h1>GLesCrocs</h1>
+
+      <a href="http://localhost:3000">Reserve ton plat </a>
+    </div>
+  );
+}
+
+export default App;
